87f22d61b861e0baeaa59b00c256181fe2f90cc0
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-12-16 23:14:00 -0800

    Fix record accessor names.

--- Loller.hs
+++ Loller.hs
@@ -39,17 +39,17 @@ type Build = [Item]
 
 data Stats = Stats { price :: Int
                    , health :: Int
-                   , hRegen :: Int
+                   , healthRegen :: Int
                    , mana :: Int
-                   , mRegen :: Int
-                   , aD :: Int
-                   , aP :: Int
+                   , manaRegen :: Int
+                   , attackDamage :: Int
+                   , abilityPower :: Int
                    , armor :: Int
-                   , mResist :: Int
+                   , magicResist :: Int
                    , lifeSteal :: Int
                    , spellVamp :: Int
-                   , aS :: Int
-                   , critChance :: Int
+                   , attackSpeed :: Int
+                   , criticalChance :: Int
                    , movementSpeed :: Int }
     deriving (Show)
 
@@ -61,31 +61,32 @@ stats :: Stats
 stats = Stats 0 0 0 0 0 0 0 0 0 0 0 0 0 0
 
 statsFor :: Item -> Stats
-statsFor AmplifyingTome = stats { price = 435, aP = 20 }
-statsFor BFSword = stats { price = 1650, aD = 45 }
-statsFor BlastingWand = stats { price = 860, aP = 40 }
+statsFor AmplifyingTome = stats { price = 435, abilityPower = 20 }
+statsFor BFSword = stats { price = 1650, attackDamage = 45 }
+statsFor BlastingWand = stats { price = 860, abilityPower = 40 }
 statsFor BootsOfSpeed = stats { price = 300, movementSpeed = 1 }
-statsFor BrawlersGloves = stats { price = 400, critChance = 8 }
+statsFor BrawlersGloves = stats { price = 400, criticalChance = 8 }
 statsFor ChainVest = stats { price = 700, armor = 45 }
 statsFor ClothArmor = stats { price = 300, armor = 18 }
-statsFor CloakOfAgility = stats { price = 830, critChance = 18 }
-statsFor Dagger = stats { price = 420, aS = 15 }
+statsFor CloakOfAgility = stats { price = 830, criticalChance = 18 }
+statsFor Dagger = stats { price = 420, attackSpeed = 15 }
 statsFor DoransBlade =
-    stats { price = 475, health = 100, aD = 10, lifeSteal = 3 }
-statsFor DoransRing = stats { price = 475, health = 100, mRegen = 7, aP = 15 }
+    stats { price = 475, health = 100, attackDamage = 10, lifeSteal = 3 }
+statsFor DoransRing = stats
+    { price = 475, health = 100, manaRegen = 7, abilityPower = 15 }
 statsFor DoransShield =
-    stats { price = 475, health = 120, hRegen = 120, armor = 10 }
-statsFor FaerieCharm = stats { price = 180, mRegen = 3 }
+    stats { price = 475, health = 120, healthRegen = 120, armor = 10 }
+statsFor FaerieCharm = stats { price = 180, manaRegen = 3 }
 statsFor GiantsBelt = stats { price = 1110, health = 430 }
-statsFor LongSword = stats { price = 410, aD = 10 }
-statsFor MekiPendant = stats { price = 390, mRegen = 7 }
-statsFor NeedlesslyLargeRod = stats { price = 1600, aP = 80 }
-statsFor NegatronCloak = stats { price = 740, mResist = 48 }
-statsFor NullMagicMantle = stats { price = 400, mResist = 24 }
-statsFor Pickaxe = stats { price = 975, aD = 25 }
-statsFor RecurveBow = stats { price = 1050, aS = 40 }
-statsFor RegrowthPendant = stats { price = 435, hRegen = 15 }
-statsFor RejuvenationBead = stats { price = 250, hRegen = 8 }
+statsFor LongSword = stats { price = 410, attackDamage = 10 }
+statsFor MekiPendant = stats { price = 390, manaRegen = 7 }
+statsFor NeedlesslyLargeRod = stats { price = 1600, abilityPower = 80 }
+statsFor NegatronCloak = stats { price = 740, magicResist = 48 }
+statsFor NullMagicMantle = stats { price = 400, magicResist = 24 }
+statsFor Pickaxe = stats { price = 975, attackDamage = 25 }
+statsFor RecurveBow = stats { price = 1050, attackSpeed = 40 }
+statsFor RegrowthPendant = stats { price = 435, healthRegen = 15 }
+statsFor RejuvenationBead = stats { price = 250, healthRegen = 8 }
 statsFor RubyCrystal = stats { price = 475, health = 180 }
 statsFor SapphireCrystal = stats { price = 400, mana = 200 }
 statsFor VampiricScepter = stats { price = 450, lifeSteal = 12 }
@@ -95,17 +96,17 @@ addStats :: Stats -> Stats -> Stats
 addStats first second = let
     p = price first + price second
     h = health first + health second
-    hr = hRegen first + hRegen second
+    hr = healthRegen first + healthRegen second
     m = mana first + mana second
-    mr = mRegen first + mRegen second
-    ad = aD first + aD second
-    ap = aP first + aP second
+    mr = manaRegen first + manaRegen second
+    ad = attackDamage first + attackDamage second
+    ap = abilityPower first + abilityPower second
     a = armor first + armor second
-    mres = mResist first + mResist second
+    mres = magicResist first + magicResist second
     steal = lifeSteal first + lifeSteal second
     vamp = spellVamp first + spellVamp second
-    as = aS first + aS second
-    cc = critChance first + critChance second
+    as = attackSpeed first + attackSpeed second
+    cc = criticalChance first + criticalChance second
     ms = max (movementSpeed first) (movementSpeed second)
     in Stats p h hr m mr ad ap a mres steal vamp as cc ms
 
