d718a0a39ed4bb2a692022a36b8429f658c12bb1
Corbin Simpson <simpsoco@osuosl.org>
2011-12-27 16:58:26 -0800

    Lol/Bravery: Nub the builds to keep all of the items unique.

--- Lol/Bravery.hs
+++ Lol/Bravery.hs
@@ -10,13 +10,16 @@ import Lol.Items
 data Bravery = Bravery { bBuild :: Build }
     deriving (Show)
 
+-- Yes, I know that this nubs, but quadratic time doesn't hurt that bad when
+-- there's only 6 items being taken.
 randomBuild :: RandomGen g => g -> Build
-randomBuild = sort . take 6 . randoms
+randomBuild = sort . take 6 . nub . randoms
 
 bootsBuild :: RandomGen g => g -> Build
 bootsBuild gen =
+    -- Remember, randomR takes a *range*, this is the A-Z of boots
     let (bootItem, gen') = randomR (BerserkersGreaves, SorcerorsShoes) gen
-        otherItems = filter (not . isBoots) $ randoms gen'
+        otherItems = nub $ filter (not . isBoots) $ randoms gen'
     in (bootItem :) $ sort $ take 5 otherItems
 
 makeBravery :: IO Bravery
