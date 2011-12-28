593c529a5e8f7dc5f5b60b5b9d0c0725faad9cac
Corbin Simpson <simpsoco@osuosl.org>
2011-12-27 17:18:07 -0800

    Handler/Root: Link to the Bravery page.

--- Handler/Root.hs
+++ Handler/Root.hs
@@ -12,6 +12,5 @@ import Import
 getRootR :: Handler RepHtml
 getRootR = do
     defaultLayout $ do
-        h2id <- lift newIdent
-        setTitle "lollerskates homepage"
+        setTitle "Lollerskates!"
         $(widgetFile "homepage")
