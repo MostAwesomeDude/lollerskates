412a8b63b6380ef91759de3b24e7daca8114729c
Corbin Simpson <simpsoco@osuosl.org>
2011-12-29 12:27:42 -0800

    Handler/Bravery: Simplify logic for buildWidget.

--- Handler/Bravery.hs
+++ Handler/Bravery.hs
@@ -19,14 +19,14 @@ $forall (index, i) <- zipped
 |]
 
 buildWidget :: [Item] -> Widget
-buildWidget is
-    | not (null is) && isBoots (head is) = [whamlet|
+buildWidget items@(i:is)
+    | isBoots i = [whamlet|
 <ul>
-    <li>Boots: #{prettyItem $ head is}
-    ^{itemsWidget $ tail is}
+    <li>Boots: #{prettyItem i}
+    ^{itemsWidget is}
 |]
-    | not (null is) = [whamlet|<ul>^{itemsWidget is}|]
-    | otherwise = [whamlet|<p>Implementation error: buildWidget: empty list|]
+    | otherwise = [whamlet|<ul>^{itemsWidget items}|]
+buildWidget [] = [whamlet|<p>Implementation error: buildWidget: empty list|]
 
 braveryWidget :: Bravery -> Widget
 braveryWidget b = [whamlet|
