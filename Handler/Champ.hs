899656c965391a1225d15d9a30eabeb9a9820806
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-12-30 16:42:58 -0800

    Handler/Champ: Add in level selection.

--- Handler/Champ.hs
+++ Handler/Champ.hs
@@ -10,20 +10,25 @@ import Lol.Stats
 
 import Widget
 
-data ChampParams = ChampParams { cpChamp :: Champ }
+data ChampParams = ChampParams { cpChamp :: Champ, cpLevel :: Level }
 
-champList :: [(Text, Champ)]
-champList = let champList = [Ahri ..]
+champChoices :: [(Text, Champ)]
+champChoices = let champList = [Ahri ..]
     in zip (map (T.pack . show) champList) champList
+
+levelChoices :: [(Text, Int)]
+levelChoices = let levelList = [1 .. 18]
+    in zip (map (T.pack . show) levelList) levelList
 
 champForm :: AForm LollerSite LollerSite ChampParams
 champForm = ChampParams
-    <$> areq (selectField champList) "Champion" Nothing
+    <$> areq (selectField champChoices) "Champion" Nothing
+    <*> areq (selectField levelChoices) "Level" (Just 18)
 
-champWidget :: Champ -> Widget
-champWidget c = [whamlet|
-<h2>#{show c} at Level 18
-^{champStatsWidget $ champStats c 18}
+champWidget :: Champ -> Level -> Widget
+champWidget c l = [whamlet|
+<h2>#{show c} at Level #{l}
+^{champStatsWidget $ champStats c l}
 |]
 
 getChampR :: Handler RepHtml
@@ -31,7 +36,7 @@ getChampR = do
     ((results, form), enctype) <- runFormGet $ renderDivs champForm
     defaultLayout $ do
         setTitle "Lollerskates ~ Champion Statistics"
-        champ <- case results of
-            FormSuccess cp -> return $ cpChamp cp
-            _ -> return Ahri
+        (champ, level) <- case results of
+            FormSuccess cp -> return $ (cpChamp cp, cpLevel cp)
+            _ -> return (Ahri, 18)
         $(widgetFile "champ")
