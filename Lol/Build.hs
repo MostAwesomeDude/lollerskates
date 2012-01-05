faa77d82e08cd52512c42993f7085d69679680b0
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-01-05 02:10:59 -0800

    Flesh out Build type somewhat.

--- Lol/Build.hs
+++ Lol/Build.hs
@@ -1,12 +1,33 @@
 module Lol.Build where
 
+import Prelude
+
 import Data.Lens.Template
 
 import Lol.Champs
 import Lol.Items
+import Lol.Stats
+
+type BuildItems = (Item, Item, Item, Item, Item, Item)
 
 data Build = Build { _bChamp :: Champ
                    , _bLevel :: Level
-                   , _bItems :: (Item, Item, Item, Item, Item, Item) }
+                   , _bItems :: BuildItems
+                   , _bChampStats :: ChampStats }
 
 $( makeLens ''Build )
+
+-- | Pack build items into a tuple for Build usage.
+packBuildItems :: [Item] -> BuildItems
+packBuildItems ([a,b,c,d,e,f]) = (a,b,c,d,e,f)
+packBuildItems _ =
+    error "Implementation error: packBuildItems called with /= 6 items"
+
+-- Icky but will be cleaned up later.
+makeChampStats :: Champ -> Level -> [Item] -> ChampStats
+makeChampStats c l is = finalizeStats $ applyBuild is $ champStats c l
+
+makeBuild :: Champ -> Level -> [Item] -> Build
+makeBuild c l is =
+    let stats = makeChampStats c l is
+    in Build c l (packBuildItems is) stats
