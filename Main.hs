4decd4b613fb5abc7d780f20ba2c52b7792b7f2c
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-01-07 12:16:19 -0800

    Rewrite Main to test out new constraint code.

--- Main.hs
+++ Main.hs
@@ -1,84 +1,38 @@
-{-# LANGUAGE DeriveDataTypeable #-}
-
 module Main where
 
-import Control.Monad
-import Data.Char
-import Data.Maybe
-import qualified Data.Map as Map
-import System.Console.CmdArgs
+import Prelude hiding ((.))
 
-import FD
-import Items
-import Loller
+import Control.Category
+import Data.Lens.Common
+import Data.Ord
 
-data Flag = Unique
-    deriving (Eq, Show)
+import Lol.Build
+import Lol.Champs
+import Lol.Constraints
+import Lol.FD
+import Lol.Items
+import Lol.Stats
+import Lol.Stats.Types
 
-data ModeParameters = BuildMode { bmUnique :: Bool
-                                , bmAttribute :: String
-                                , bmParameters :: [String] }
-                    | ItemMode { imAttribute :: String }
-    deriving (Data, Show, Typeable)
+improvesDPS :: Item -> Bool
+improvesDPS i = let stats = itemStats i
+    in any (> 0) $ map (^$ stats) [csAttackDamage . iCoreStats
+                                  ,esBonusAttackSpeed . iExtendedStats
+                                  ,esCriticalChance . iExtendedStats]
 
-arguments :: Mode (CmdArgs ModeParameters)
-arguments = cmdArgsMode $
-    modes [ BuildMode { bmUnique = False &= name "unique"
-                            &= help "Force wildcard items to be unique"
-                      , bmAttribute = def &= argPos 0 &= typ "ATTRIBUTE"
-                      , bmParameters = def &= args &= typ "ITEMS" }
-                      &= name "build"
-          , ItemMode { imAttribute = def &= argPos 0 &= typ "ATTRIBUTE" }
-                     &= name "item" ]
-    &= summary "Lollerskates"
-
-maybeRead :: Read a => String -> Maybe a
-maybeRead = fmap fst . listToMaybe . reads
-
--- | Given a padding and a list, pad (or truncate!) the list to a certain
---   length.
-pad :: Int -> a -> [a] -> [a]
-pad len padding l = take len $ l ++ replicate len padding
-
-lookupAttribute :: Monad m => String -> m Comparator
-lookupAttribute attr = case Map.lookup (map toLower attr) attributeFilters of
-    Just f -> return f
-    Nothing -> fail $ "Couldn't match attribute " ++ attr
-
-lookupItem :: Monad m => String -> m [Item]
-lookupItem "*" = return [Empty ..]
-lookupItem name = case maybeRead name of
-    Just item -> return [item]
-    Nothing -> fail $ "Couldn't match item name " ++ name
-
-parseArguments :: Monad m => [String] -> m (Comparator, [[Item]])
-parseArguments args = do
-    when (null args) $ fail "No arguments given!"
-    attribute <- lookupAttribute $ head args
-    items <- mapM lookupItem $ pad 6 "*" (tail args)
-    return (attribute, items)
-
-buildForFlags :: [Flag] -> [[Item]] -> [Build]
-buildForFlags flags items = runFD $ do
-    build <- builds items
-    when (Unique `elem` flags) $ withVariety build
+buildList :: [[Item]]
+buildList = runFD $ do
+    build <- defaultBuilds
+    head build `satisfies` isBoots
+    mapM_ (flip lacksValue Empty) (tail build)
+    mapM_ (flip satisfies improvesDPS) (tail build)
     labelling build
+
+createBestBuild :: Build
+createBestBuild = let bs = map (makeBuild Tryndamere 18) buildList
+    in maximumBy' (comparing $ dps . getL bChampStats) bs
 
 main :: IO ()
 main = do
-    args <- cmdArgsRun arguments
-    doMode args
-
-doMode :: ModeParameters -> IO ()
-doMode (BuildMode isUnique attr params) = do
-    attribute <- lookupAttribute attr
-    sets <- mapM lookupItem $ pad 6 "*" params
-    let flags = if isUnique then [Unique] else []
-    let build = buildForFlags flags sets
-    when (null build) $ fail
-        $ "No builds match the given constraints: " ++ show params
-    print $ maxBuild attribute build
-doMode (ItemMode attr) = do
-    attribute <- lookupAttribute attr
-    print $ bestItem attribute [Empty ..]
-doMode _ = fail $ "Unknown mode. You probably shouldn't be able to reach this."
+    putStrLn "What's up?"
+    putStrLn $ show createBestBuild
